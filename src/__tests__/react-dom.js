0833e98ee9b79a5fda9e1b335c31865568a920b3
faustaleonardo <faustaleonardo11@gmail.com>
2023-10-07 19:38:42 +0700

    course 12
    Signed-off-by: faustaleonardo <faustaleonardo11@gmail.com>

--- src/__tests__/react-dom.js
+++ src/__tests__/react-dom.js
@@ -1,10 +1,28 @@
 import * as React from 'react'
-import ReactDOM from 'react-dom'
 import {FavoriteNumber} from '../favorite-number'
+import {render} from '@testing-library/react'
+import user from '@testing-library/user-event'
 
 test('renders a number input with a label "Favorite Number"', () => {
-  const div = document.createElement('div')
-  ReactDOM.render(<FavoriteNumber />, div)
-  expect(div.querySelector('input').type).toBe('number')
-  expect(div.querySelector('label')).toHaveTextContent('Favorite Number')
+  const {getByLabelText} = render(<FavoriteNumber />)
+  const input = getByLabelText(/favorite number/i)
+  expect(input).toHaveAttribute('type', 'number')
 })
+
+test('entering an invalid value shows an error message', () => {
+  const {getByLabelText, getByRole, rerender, queryByRole} = render(
+    <FavoriteNumber />,
+  )
+  const input = getByLabelText(/favorite number/i)
+  user.type(input, '10')
+  expect(getByRole('alert')).toHaveTextContent(/the number is invalid/i)
+  rerender(<FavoriteNumber max={10} />)
+  expect(queryByRole('alert')).toBe(null)
+})
+
+// disabled for the purpose of this lesson. We'll get to this later
+/*
+eslint
+  testing-library/no-dom-import: "off",
+  testing-library/prefer-screen-queries: "off"
+*/
